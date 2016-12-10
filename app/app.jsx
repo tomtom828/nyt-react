233744aa75eb2fc3668194d03cb8c4d67a404a83
tomtom28 <tomthompson828@gmail.com>
2016-12-10 00:04:12 -0500

    added bundle script and transpile is working

--- app/app.jsx
+++ app/app.jsx
@@ -4,4 +4,13 @@
 
 
 
-// Require Node Modules
+// Include the Main React Dependencies
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Include the main Parent Component
+var Main = require("./components/children/Main.jsx");
+
+// This code here allows us to render our main component (in this case Main)
+// Note that the Id is "app" which matches that of the "index.html" file
+ReactDOM.render(<Main />, document.getElementById("app"));
--- app/components/children/Main.jsx
+++ app/components/children/Main.jsx
@@ -1,2 +1,45 @@
 // Contains the main-container div that holds the main layout and navigation. 
 // This component should also be able to hold sub-components Search and Saved
+
+// Include React
+var React = require("react");
+
+// UN COMMENT ALL THESE LATER!
+// Here we include all of the sub-components
+//var Search = require("./Search");
+//var Saved = require("./Saved");
+
+// Requiring our helper for making API calls
+//var helpers = require("../utils/helpers");
+
+// Create the Parent Component
+var Main = React.createClass({
+
+  // Here we set a generic state associated with the number of clicks
+  getInitialState: function() {
+    return {
+      test: 0
+    };
+  },
+
+  // NEED TO INCLUDE MORE FUNCTIONS HERE
+
+  // Here we render the function
+  render: function() {
+    return (
+      <div className="container">
+
+        <div className="row">
+
+          <div className="jumbotron">
+            <h2>New York Times <b>React</b>ions!</h2>
+            <hr />
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+// Export the component back for use in other files
+module.exports = Main;
