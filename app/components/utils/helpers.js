f45b3093d92c41f90f3a09c103aa6781bf4140c5
tomtom28 <tomthompson828@gmail.com>
2016-12-15 20:37:03 -0500

    refactored API call to use axios node package

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -1,41 +1,48 @@
 // Node Dependencies
-var request = require('request');
+var axios = require('axios');
 
 
 // NY Times API Request Function
-var articleQuery = function(topic, beginYear, endYear, callback){
+var articleQuery = function(topic, beginYear, endYear){
 
-  request.get({
-    url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
-    qs: {
-      'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931", // API Key ripped from Ahmed's copy on Heroku muahahaha
-      'q': topic,
-      'begin_date': beginYear + "0101", // for Jan 1st of start year
-      'end_date': endYear + "1231" // for Dec 31st of end year
-    },
-  }, function(err, response, body) {
+  var authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
 
-    // Parse for JSON
-    body = JSON.parse(body);
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=" +
+                  topic + "&begin_date=" + beginYear + "0101" + "&end_date=" + endYear + "1231";
 
-    // Use callback function
-    callback(body);
+  // API get request
+  axios.get(queryURL).then(function(response) {
 
+    var result = [];
+
+    // If get get a result, return only the first 5 articles
+    if (response.data.response.docs[0]) {
+
+      for(var i=0; i<response.data.response.docs.length; i++){
+        // Break out of the loop if there are more than 5 entries
+        
+        if(i==5){
+          break;
+        }
+        else {
+          // Otherwise, push to results array
+          result.push(response.data.response.docs[i]);
+        }
+      }
+
+      // Return the array of articles
+      return result;
+    }
+    
+    // If we don't get any results, return an empty string
+    return "";
   });
 
 }
 
 
-// Export all helper functions
+
+//Export all helper functions
 module.exports = {
-  articleQuery
+ articleQuery
 }
-
-
-
-
-
-// TEST CASE IGNORE
-// articleQuery("hello", 1991, 1992, function(data){
-//   console.log(data);
-// })
